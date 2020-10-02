c32258f94a1ee79776ba1df2cfe901c71f160ba0
Quocthai <voquocthai0301@gmail.com>
2020-10-02 19:42:26 +0700

    thai up file

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 
 class header extends Component {
-    render() {
-        return (
-            <div>
-                var NewComponent = React.createClass({
-  render: function() {
+  render() {
     return (
-
-      <header>
-        {/*- Navbar -*/}
-        <div className="root">
+      <div>
+        <div>
+          {/*- Navbar -*/}
           <nav id="nav1" className="navbar navbar-expand-lg">
             <div className="container">
               <a className="navbar-brand text-white" href="#"><img src="https://secureservercdn.net/198.71.233.138/d0y.65e.myftpupload.com/wp-content/uploads/2019/08/header-logo.png" alt="" height="50px" width="170px" /></a>
@@ -26,37 +21,52 @@ class header extends Component {
                     <a id="header-a" className="nav-link" href="#">Solutions</a>
                   </li>
                   <li className="nav-item pl-1">
-                    <a id="header-a" className="nav-link" href="QT-community.html">QT-Community
-                    </a>
+                    <a id="header-a" className="nav-link" href="#">QT-Community
+                  </a>
                   </li>
                   <li className="nav-item pl-1">
-                    <a id="header-a" className="nav-link" href="forums.html">Forums
-                    </a>
+                    <a id="header-a" className="nav-link" href="#">Forums
+                  </a>
                   </li>
                   <li className="nav-item pl-1">
                     <a id="header-a" className="nav-link" href="#">Resources
-                    </a>
+                  </a>
                   </li>
                   <li className="nav-item pl-1">
                     <a id="header-a" className="nav-link" href="#">Shop
-                    </a>
+                  </a>
                   </li>
                   <li className="nav-item pl-1">
                     <a id="header-a" className="nav-link" href="#">Account
-                    </a>
+                  </a>
                   </li>
                 </ul>
               </div>
+              
             </div>
           </nav>
           {/*# Navbar #*/}
-        </div></header>
+          <nav className="row">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light col" id="navbar1">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent ">
+            <h4 className="Acc">QT-Forums</h4>
+            <ul className="navbar-nav ml-auto" style={{marginRight: '200px'}}>
+              <li className="nav-item">
+                <a href className="nav-link" style={{marginTop: '90px'}}>Home /</a>
+              </li>
+              <li className="nav-item">
+                <a href className="nav-link" style={{marginTop: '90px'}}>QT-Forums </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </nav>
+        </div>
+      </div>
     );
   }
-});
-            </div>
-        );
-    }
 }
 
 export default header;
