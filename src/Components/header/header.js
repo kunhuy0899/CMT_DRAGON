fcb780827e250fe1fb7b3477678648a7c33340cd
Quocthai <voquocthai0301@gmail.com>
2020-10-03 10:42:56 +0700

    quocthai up file

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -46,23 +46,7 @@ class header extends Component {
             </div>
           </nav>
           {/*# Navbar #*/}
-          <nav className="row">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light col" id="navbar1">
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent ">
-            <h4 className="Acc">QT-Forums</h4>
-            <ul className="navbar-nav ml-auto" style={{marginRight: '200px'}}>
-              <li className="nav-item">
-                <a href className="nav-link" style={{marginTop: '90px'}}>Home /</a>
-              </li>
-              <li className="nav-item">
-                <a href className="nav-link" style={{marginTop: '90px'}}>QT-Forums </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </nav>
+          
         </div>
       </div>
     );
