5d14544537136935950c682f8774e819254f5295
kunhuy0899 <55186853+kunhuy0899@users.noreply.github.com>
2020-10-05 14:39:50 +0700

    giao dien admin

---
+++ admin/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Content from './Components/Content/Content'
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
+import Menu from './Components/Menu/Menu'
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
